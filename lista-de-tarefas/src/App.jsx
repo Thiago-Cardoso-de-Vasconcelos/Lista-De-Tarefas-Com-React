d4816e7b9867aaf972e570238f5e32367daab7e7
Thiago Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2023-09-28 18:47:44 -0300

    feat: Adicionando funcionalidade de criar tarefa

--- lista-de-tarefas/src/App.jsx
+++ lista-de-tarefas/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Todo from "./components/Todo";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -24,14 +25,28 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random()* 1000),
+        text,
+        category,
+        inCompleted: false,
+      }
+    ];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
--- lista-de-tarefas/src/components/Todo.jsx
+++ lista-de-tarefas/src/components/Todo.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 
-const Todo = ({todo}) => {
+const Todo = ({ todo }) => {
   return (
     <div className="todo">
       <div className="content">
@@ -8,8 +7,8 @@ const Todo = ({todo}) => {
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button>Completar</button>
-        <button>Deletar</button>
+        <button className="complete">Completar</button>
+        <button className="remove">Deletar</button>
       </div>
     </div>
   );
