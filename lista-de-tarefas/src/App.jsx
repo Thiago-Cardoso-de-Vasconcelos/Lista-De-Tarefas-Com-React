e4255502acec2ae3c1ff751d3607f8d683d3cf59
Thiago Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2023-09-28 05:47:13 -0300

    feat: Criando a estrutura básica do projeto

--- lista-de-tarefas/src/App.jsx
+++ lista-de-tarefas/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import Todo from "./components/Todo";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar Funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompletd: false,
+    },
+    {
+      id: 2,
+      text: "Fazer exercícios",
+      category: "Pessoal",
+      isCompletd: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompletd: false,
+    },
+  ]);
 
   return (
-    <>Feito a Limpeza!</>
-  )
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
